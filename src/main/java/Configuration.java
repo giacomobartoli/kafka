4c398cd840b4a2db8dbef303c258c1fac4cc660f
Giacomo Bartoli <giacomo.bartoli3@studio.unibo.it>
2018-02-18 15:46:42 +0100

    minor

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -1,9 +1,9 @@
 public class Configuration {
 
-    public static String consumerKey = "aWNBhBhOBnVZJrttHUpFVF3vI";
-    public static String consumerSecret = "7QlW5LRQSnorei7aPty8pM3V48SIuNdF9n9KkYLFqch0pfITjj";
-    public static String accessToken = "2366239772-m0dtL0wWdisuO7ac0IUgfH2M3c2dhTGZRp2NS5c";
-    public static String accessTokenSecret = "27ttqxVdqZbo6CRNkr0yHXLQpT3Un3lAduMWBgKXdEN0O";
-    public static String topicName = "tweets";
-    public static String hashtag = "Trump";
+    public static String consumerKey = "YOUR_KEY_HERE";
+    public static String consumerSecret = "YOUR_KEY_HERE";
+    public static String accessToken = "YOUR_KEY_HERE";
+    public static String accessTokenSecret = "YOUR_KEY_HERE";
+    public static String topicName = "YOUR_TOPIC_HERE";
+    public static String hashtag = "YOUR_HASHTAG_HERE";
 }
